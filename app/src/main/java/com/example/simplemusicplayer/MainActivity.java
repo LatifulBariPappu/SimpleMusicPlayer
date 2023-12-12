c1cd7683bb04bc78c98e94d78daf11da904df940
Latiful_Bari_Pappu <latifulbari2001@gmail.com>
2023-12-12 22:01:17 +0600

    mediaPlayer

--- app/src/main/java/com/example/simplemusicplayer/MainActivity.java
+++ app/src/main/java/com/example/simplemusicplayer/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.simplemusicplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MediaPlayer mp=new MediaPlayer();
+        mp.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        String aPath="android.resourse://"+getPackageName()+"/raw/amaydekona";
+        Uri audioURI= Uri.parse(aPath);
+        try{
+            mp.setDataSource(this,audioURI);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        mp.prepare();
     }
 }
